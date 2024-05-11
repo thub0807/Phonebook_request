527e0b89a984de45ffee59f5c90fe22fcb789789
Levi <1114104133@qq.com>
2024-05-11 20:20:53 +0800

    fix  read dist

--- index.js
+++ index.js
@@ -19,28 +19,8 @@ app.use(cors())
 
 let notes = 
   [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
 ]
-
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
